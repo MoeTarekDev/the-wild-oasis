72a60140c0a1ea6bbd8effe66acde3edbe1fb0fc
Mohamed Tarek <moetarekdev@gmail.com>
2024-09-11 00:00:31 +0300

    update style

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <div className="flex flex-col gap-10 mt-10 items-center text-accent-100">
+    <div className="flex flex-col gap-10 mt-10 items-center container mx-auto px-5 text-accent-100">
       <h2 className="text-3xl font-semibold">
         Sign in to access your guest area
       </h2>
